25d1b581da838dd4ff5505d8c8573fc31ca19e01
Rahulgodaraa <rgodara152001@gmail.com>
2022-10-30 01:11:41 +0530

    day 5

--- section 8/functions.js
+++ section 8/functions.js
@@ -1,18 +1,51 @@
 'use strict';
-const  bookedflights = []
+// const  bookedflights = []
 
-const booking = function (flight = 'AIR INDIA' , numberofpassenger = 4 , place = 'KISHANGARH' ) {
-   const bookflight = 
-   {
-    flight ,
-    numberofpassenger , 
-    place  
-   }
-   console.log(bookflight);
-   bookedflights.push(bookflight);
-};
+// const booking = function (flight = 'AIR INDIA' , numberofpassenger = 4 , place = 'KISHANGARH' ) {
+//    const bookflight = 
+//    {
+//     flight ,
+//     numberofpassenger , 
+//     place  
+//    }
+//    console.log(bookflight);
+//    bookedflights.push(bookflight);
+// };
 
-booking("EMIRATES");
+// booking("EMIRATES");
 
 //---------------------------------Higherorder function 
 
+const FirstletterUpper = function (str) {
+   const [first , ...other] = str.split(' ');
+   return [first.toUpperCase(), ...other].join(' ');
+};
+
+
+// higher function that calls another function in it
+
+const transformer = function (str,fn) {
+   console.log(`Original string: ${str}`);
+   console.log(`This is transformed : ${fn(str)}`);
+
+
+}
+
+transformer('rahul choudhary' , FirstletterUpper);
+
+//-----------------------------------------------------function return function
+
+const greeting = function (greetings) {
+    return function (name) {
+      console.log(`${greetings} ${name}`)
+    }
+}
+
+const greetme = greeting('Greetings mr.');
+greetme('Rahul');
+greetme('Rameshwar');
+
+//-------------------------------------------------arrow function return arrow function
+
+const greet = greetingss => names => console.log(`${greetingss} ${names}`);
+greet('rahul' )( 'greet');
